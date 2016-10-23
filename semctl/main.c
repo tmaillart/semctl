36712d6ed172668a5a62110a32fd6a70b1bb8112
Théo MAILLART <theo.maillart@etu.utc.fr>
2016-10-23 14:30:35 +0200

    Initial Commit

---
+++ semctl/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  semctl
+//
+//  Created by Théo MAILLART on 23/10/2016.
+//  Copyright © 2016 Théo MAILLART. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
